f88f768d3d23545e97a3ced39c5fa4fdf0db8543
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-03-22 06:31:03 +0500

    new file

---
+++ fileName.ts
@@ -0,0 +1,7 @@
+let userName1: string = "Abdullah"
+let age1 : number = 20
+let userName2 : string= "Ali"
+let age2 : number = 35;
+console.log(userName1+ " is "+age1+" years old.")
+console.log("------------------------------")
+console.log(userName2 + "is "+age2+" years old.")
